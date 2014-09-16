4e5162cbed75caa654aa9b05687a1f314bc37281
Cristina Matonte <anitsirc1@gmail.com>
2014-09-16 19:19:59 +0200

    Include Mergesort class in ClosestPair
    We are allowed to upload only one file, so I include the Mergesort class inside ClosestPair

--- src/ClosestPairs.java
+++ src/ClosestPairs.java
@@ -213,6 +213,73 @@ public class ClosestPairs {
         }
     }
 
+    public static class Mergesort {
+        private Node[] numbers;
+        private Node[] helper;
+        private String axis;
+
+        private int number;
+
+        public void sort(Node[] values, String axis) {
+            this.numbers = values;
+            number = values.length;
+            this.helper = new Node[number];
+            this.axis = axis;
+            mergesort(0, number - 1);
+        }
+
+        // low and high are indexes
+        private void mergesort(int low, int high) {
+            // check if low is smaller then high, if not then the array is sorted
+            if (low < high) {
+                // Get the index of the element which is in the middle
+                int middle = low + (high - low) / 2;
+                // Sort the left side of the array
+                mergesort(low, middle);
+                // Sort the right side of the array
+                mergesort(middle + 1, high);
+                // Combine them both
+                merge(low, middle, high);
+            }
+        }
+
+        private boolean comparePoints(Point2D.Double pointI, Point2D.Double pointJ) {
+            if (axis.equalsIgnoreCase("x")) {
+                return pointI.getX() <= pointJ.getX();
+            } else {
+                return pointI.getY() <= pointJ.getY();
+            }
+        }
+
+        private void merge(int low, int middle, int high) {
+            // Copy both parts into the helper array
+            for (int i = low; i <= high; i++) {
+                helper[i] = numbers[i];
+            }
+            int i = low;
+            int j = middle + 1;
+            int k = low;
+            // Copy the smallest values from either the left or the right side back
+            // to the original array
+            while (i <= middle && j <= high) {
+                if (comparePoints(helper[i].point, helper[j].point)) {
+                    numbers[k] = helper[i];
+                    i++;
+                } else {
+                    numbers[k] = helper[j];
+                    j++;
+                }
+                k++;
+            }
+            // Copy the rest of the left side of the array into the target array
+            while (i <= middle) {
+                numbers[k] = helper[i];
+                k++;
+                i++;
+            }
+        }
+    }
+
     private static class CustomException extends Throwable {
         public CustomException(String message) {
             System.out.println(message);
